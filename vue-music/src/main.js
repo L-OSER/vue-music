e490cb1c1aedd0bddbecffac2fd0d4e7ffc1df73
周嘉俊 <770236524@qq.com>
2018-11-13 16:49:19 +0800

    初始化目录

--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -1,8 +1,14 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
+
+import 'common/stylus/index.styl'
+/* 解决移动端点击三百毫秒的延迟 */
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
