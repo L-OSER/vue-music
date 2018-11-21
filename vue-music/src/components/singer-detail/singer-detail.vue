94fd6713bffe6b69d39096cd237a6048343410c5
周嘉俊 <770236524@qq.com>
2018-11-21 23:40:47 +0800

    增加歌手详情列表

--- vue-music/src/components/singer-detail/singer-detail.vue
+++ vue-music/src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,7 @@
 <template>
   <transition name="slide">
-  <div class="singer-detail"></div>
+  <!--<div class="singer-detail"></div>-->
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -9,6 +10,7 @@ import {mapGetters} from 'vuex'
 import {getSingerDetail, getMusic} from 'api/singer'
 import {ERR_OK} from 'api/config'
 import {createSong} from 'common/js/song'
+import MusicList from 'components/music-list/music-list'
 
 export default {
   data() {
@@ -18,6 +20,12 @@ export default {
     }
   },
   computed: {
+    title() {
+      return this.singer.name
+    },
+    bgImage() {
+      return this.singer.avatar
+    },
     ...mapGetters([
       'singer'
     ])
@@ -54,21 +62,15 @@ export default {
       })
       return this.retlist
     }
+  },
+  components: {
+    MusicList
   }
 }
 </script>
 
 <style scoped lang="stylus">
 @import "~common/stylus/variable"
-
-.singer-detail
-  position: fixed
-  z-index: 100
-  top: 0
-  left: 0
-  right: 0
-  bottom: 0
-  background: $color-background
 
 .slide-enter-active, .slide-leave-active
   transition: all 0.3s
