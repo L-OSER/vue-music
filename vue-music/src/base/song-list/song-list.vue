da0b4a9f722ca92ccd31a06d8ee567daa9395733
周嘉俊 <770236524@qq.com>
2019-01-12 17:32:52 +0800

    添加排名样式

--- vue-music/src/base/song-list/song-list.vue
+++ vue-music/src/base/song-list/song-list.vue
@@ -2,6 +2,9 @@
     <div class="song-list">
       <ul>
         <li @click="selectItem(song, index)" v-for="(song, index) in songs" :key="index" class="item">
+          <div class="rank" v-show="rank">
+            <span :class="getRankCls(index)">{{getRankText(index)}}</span>
+          </div>
           <div class="content">
             <h2 class="name">
               {{song.name}}
@@ -18,6 +21,10 @@ export default {
     songs: {
       type: Array,
       default: []
+    },
+    rank: {
+      type: Boolean,
+      default: false
     }
   },
   methods: {
@@ -26,6 +33,20 @@ export default {
     },
     getDesc(song) {
       return `${song.singer} , ${song.album}`
+    },
+    getRankCls(index) {
+      // 前三名返回icon样式
+      if (index <= 2) {
+        return `icon icon${index}`
+      } else {
+        return 'text'
+      }
+    },
+    getRankText(index) {
+      // 如果是后三名,返回序号
+      if (index > 2) {
+        return index + 1
+      }
     }
   }
 }
@@ -42,6 +63,25 @@ export default {
     box-sizing: border-box
     height: 64px
     font-size: $font-size-medium
+    .rank
+      flex: 0 0 25px
+      width: 25px
+      margin-right: 30px
+      text-align: center
+      .icon
+        display: inline-block
+        width: 25px
+        height: 24px
+        background-size: 25px 24px
+        &.icon0
+          bg-image('first')
+        &.icon1
+          bg-image('second')
+        &.icon2
+          bg-image('third')
+       .text
+         color: $color-theme
+         font-size: $font-size-large
     .content
       flex: 1
       line-height: 20px
